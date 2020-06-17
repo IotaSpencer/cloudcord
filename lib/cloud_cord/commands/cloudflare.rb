c5bce91ad7702516a55f0b532e36844fbb0f9cca
Ken Spencer <me@iotaspencer.me>
2020-06-17 05:27:36 -0400

    Initial Commit 2

---
+++ lib/cloud_cord/commands/cloudflare.rb
@@ -0,0 +1,10 @@
+module CloudCord
+  module Bot
+    module DiscordCommands
+      module Cloudflare
+        extend Discordrb::Commands::CommandContainer
+        command(:dns)
+      end
+    end
+  end
+end
